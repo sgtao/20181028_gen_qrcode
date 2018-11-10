6c3d15cd0aa56c844ebbf3aaebc3217b2f4583fd
shogo <sg.tao.so@gmail.com>
2018-11-10 18:43:46 +0900

    convert src from using jQuery to EventListner

--- script/gen_qrcode.js
+++ script/gen_qrcode.js
@@ -1,15 +1,27 @@
-'use strict';
+(function(){
+	'use strict';
 
-// ex) https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
-var url =  'https://api.qrserver.com/v1/create-qr-code/?';
-var charset = 'charset-source=UTF-8';
-var size = '&size=150x150';
+    // ex) https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+    
+	var url =  'https://api.qrserver.com/v1/create-qr-code/?';
+	var charset = 'charset-source=UTF-8';
+	var size = '&size=150x150';
 
-$(function(){
-    $('#button').click(function(){
-	let msg = encodeURIComponent($('#qrdata').val());
-	console.log(msg);
-	var img = $('<img>').attr('src', url + charset + size + '&data='+msg);
-	$('#barCode').html(img);
-    });
-});
+	var convert = document.getElementById('convert');
+    var qr_img = document.createElement('img');
+    var first_load = false;
+
+  convert.addEventListener('click', function(){
+	var textarea = encodeURIComponent(document.F1.qrdata.value);
+	console.log(textarea);
+
+	// var img = $('<img>').attr('src', url + charset + size + '&data=' + textarea);
+      qr_img.setAttribute("src", url + charset + size + '&data=' + textarea);
+      qr_img.setAttribute("id", "qrimg");
+      if (first_load === false) {
+          document.body.appendChild(qr_img);
+	  first_load = true;
+      } 
+
+  });
+})();
