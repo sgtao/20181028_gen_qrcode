0d3af42eb1c3356336518e0863157b5ecc5d6d58
shogo <sg.tao.so@gmail.com>
2018-11-10 19:17:28 +0900

    change style of button

--- script/gen_qrcode.js
+++ script/gen_qrcode.js
@@ -1,27 +1,27 @@
 (function(){
 	'use strict';
 
-    // ex) https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
-    
+	// ex) https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
 	var url =  'https://api.qrserver.com/v1/create-qr-code/?';
 	var charset = 'charset-source=UTF-8';
 	var size = '&size=150x150';
 
-	var convert = document.getElementById('convert');
-    var qr_img = document.createElement('img');
-    var first_load = false;
+	var textarea = document.getElementById('qrdata');
+	var conv_btn = document.getElementById('conv_btn');
+	var barCode  = document.getElementById('barCode');
+	var qr_img   = document.createElement('img');
+	var first_load = false;
 
-  convert.addEventListener('click', function(){
-	var textarea = encodeURIComponent(document.F1.qrdata.value);
-	console.log(textarea);
+	conv_btn.addEventListener('click', function(){
+		var textarea = encodeURIComponent(document.F1.qrdata.value);
+		console.log(textarea);
 
-	// var img = $('<img>').attr('src', url + charset + size + '&data=' + textarea);
-      qr_img.setAttribute("src", url + charset + size + '&data=' + textarea);
-      qr_img.setAttribute("id", "qrimg");
-      if (first_load === false) {
-          document.body.appendChild(qr_img);
-	  first_load = true;
-      } 
-
-  });
+		// var img = $('<img>').attr('src', url + charset + size + '&data=' + textarea);
+		qr_img.setAttribute("src", url + charset + size + '&data=' + textarea);
+		qr_img.setAttribute("id", "qrimg");
+		if (first_load === false) {
+			barCode.appendChild(qr_img);
+			first_load = true;
+		} 
+	  });
 })();
